54a647b20793a7bed5b31d0fe98fe428817f2e47
Will G Piro <willgpiro@gmail.com>
2020-05-13 17:48:31 -0700

    successful render to page

--- src/components/ExerciseForm.jsx/ExerciseForm.jsx
+++ src/components/ExerciseForm.jsx/ExerciseForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { postExercise } from '../../services/exerciseFetch';
 
 const ExerciseForm = () => {
@@ -6,40 +6,48 @@ const ExerciseForm = () => {
  const [description, setDescription] = useState('');
  const [sets, setSets] = useState(2);
  const [reps, setReps] = useState(6);
- const [url, setUrl] = usetState('');   
+ const [url, setUrl] = useState('');   
 
  const handleSubmit = event => {
    event.preventDefault();
    postExercise({ name, description, sets, reps, url })
- }
+    .then(exercise ={
+
+    });
+ };
 
  //On submitting an exercise, we want to add this to our local state, redux state, and our backend database. 
   return (
-    <form onSubmit={}>
+    <form onSubmit={handleSubmit}>
       <input 
         type="text" 
         value={name} 
         onChange={({ target }) => setName(target.value)} placeholder="Exercise Name" />
+
       <textarea 
         value={description} 
         onChange={({ target }) => setDescription(target.valut)} 
         placeholder="Write the description of your exercise here.">
       </textarea>
+
       <input 
         type="number" 
         value={sets} 
         onChange={({ target }) => setSets(target.value)} 
         placeholder="Enter number of sets." />
+
       <input 
         type="number" 
-        value={number} 
+        value={reps} 
         onChange={({ target }) => setReps(target.value)} 
         placeholder="Enter number of reps." />
+
       <input 
         type="text" 
         value={url} 
         onChange={({ target }) => setUrl(target.value)} 
         placeholder="Enter photo url" />
+
       <button>Add Exercise</button>
     </form>
   )
